2db8be78873bb68eac67936d155e0f0b7cc4e11e
Crow <crow@SlackNest.tree.net>
2018-02-08 20:48:27 +0100

    some improvements

--- py_slack_desc.py
+++ py_slack_desc.py
@@ -4,15 +4,42 @@ import sys
 import textwrap
 
 
+def arguments():
+    """Parses arguments
+    Returns
+    -------
+    argparse.Namespace
+        Returns Argparse Namespace
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--interactive", default=False,
+                        help="run script in interactive mode",
+                        action="store_true")
+    parser.add_argument("-o", "--output", default='',
+                        help="output directory")
+    cmd_parser = parser.add_argument_group('commandline mode')
+    cmd_parser.add_argument("-n", "--name", nargs=1, help="program name")
+    cmd_parser.add_argument("-s", "--short", nargs='+',
+                            help="program short description (one line)")
+    cmd_parser.add_argument("-d", "--description", nargs='+',
+                            help="program description")
+    cmd_parser.add_argument("-u", "--url", nargs=1,
+                            help="program homepage URL")
+    args = parser.parse_args()
+    return args
+
+
 def text_warpper(text, prefix, separator):
     """Wraps text
-
     Parameters:
     ----------
     text : {str}
         Text to be warped
     prefix : {str}
-        Defines a name of the program to be used as a prefix for every line of text
+        Defines a name of the program to be used as a prefix
+        for every line of text
     separator : {str}
         Defines the text inserted between prefix and text
     Returns
@@ -39,9 +66,9 @@ def text_warpper(text, prefix, separator):
         return warped_text.split()
 
 
-def text_validator(text, one_word=False, one_line=False, six_lines=False, pkg_name=None):
+def text_validator(text, one_word=False, one_line=False,
+                   six_lines=False, pkg_name=None):
     """Validates the text that makes up the slack-desc file
-
     Parameters:
     ----------
     text : {str}
@@ -49,17 +76,17 @@ def text_validator(text, one_word=False, one_line=False, six_lines=False, pkg_na
     one_word : {bool}, optional
         Defines if text has to be a single word (the default is False)
     one_line : {bool}, optional
-        Defines if text has to be maximally six line long (the default is False)
+        Defines if text has to be maximally six line long
+        (the default is False)
     six_lines : {bool}, optional
-        Defines if text has to be maximally six lines long  (the default is False)
+        Defines if text has to be maximally six lines long
+        (the default is False)
     pkg_name : {str}, optional
         Defines name of program (the default is None)
-
     Raises
     ------
     ValueError
         Raisers ValueError if text doesn't pass validation
-
     Returns
     -------
     bool
@@ -76,7 +103,7 @@ def text_validator(text, one_word=False, one_line=False, six_lines=False, pkg_na
     elif one_line:
         if not pkg_name:
             sys.exit("Error: unknown program name.")
-        elif (len(pkg_name + text) + 2) > 79:
+        elif (len(pkg_name) + len(text) + 2) > 79:
             raise ValueError(
                 "Error: Package short description is too long. Try again.")
     elif six_lines:
@@ -89,22 +116,25 @@ def text_validator(text, one_word=False, one_line=False, six_lines=False, pkg_na
         return True
 
 
-def user_input(question, one_word=False, one_line=False, six_lines=False, pkg_name=None):
+def user_input(question, one_word=False, one_line=False,
+               six_lines=False, pkg_name=None):
     """Asks user for input and pass it to validator
-
     Parameters:
     ----------
     question : {str}
         Content of the question asked to the user
     one_word : {bool}, optional
-        Passed to the validator, defines if user input has to be a single word (the default is False)
+        Passed to the validator, defines if user input has to be a single word
+        (the default is False)
     one_line : {bool}, optional
-        Passed to the validator, defines if user input has to be maximally six line long (the default is False)
+        Passed to the validator, defines if user input has to be maximally one
+        line long (the default is False)
     six_lines : {bool}, optional
-        Passed to the validator, defines if user input has to be maximally six lines long  (the default is False)
+        Passed to the validator, defines if user input has to be maximally six
+        lines long  (the default is False)
     pkg_name : {str}, optional
-        Passed to the validator, defines name of a program (the default is None)
-
+        Passed to the validator, defines name of a program
+        (the default is None)
     Returns
     -------
     str
@@ -124,7 +154,6 @@ def user_input(question, one_word=False, one_line=False, six_lines=False, pkg_na
 
 def header():
     """Header of slack-build file
-
     Returns
     -------
     list
@@ -138,13 +167,12 @@ def header():
         '# on the right side marks the last column you can put a character in.  You must',
         "# make exactly 11 lines for the formatting to be correct.  It's also",
         "# customary to leave one space after the ':'.",
-        '']
+        ""]
     return header
 
 
 def handy_ruler(pkg_name):
     """Creates a handy ruler
-
     Parameters:
     ----------
     pkg_name : {str}
@@ -165,113 +193,108 @@ def handy_ruler(pkg_name):
     return handy_ruler
 
 
-def arguments():
-    parser = argparse.ArgumentParser()
+def valid_path(path, file_name='', override=False):
+    # TODO: need some work
+    file_path = os.path.join(path, file_name)
+    file_path = os.path.expandvars(file_path)
+    file_path = os.path.expanduser(file_path)
+    file_path = os.path.abspath(file_path)
 
-    parser.add_argument("-i", "--interactive", default=False,
-                        help="run script in interactive mode",
-                        action="store_true")
-    parser.add_argument("-o", "--output", help="output directory")
-    cmd_parser = parser.add_argument_group('commandline mode')
-    cmd_parser.add_argument("-n", "--name", nargs=1, help="program name")
-    cmd_parser.add_argument("-s", "--short", nargs='+',
-                            help="program short description (one line)")
-    cmd_parser.add_argument("-d", "--description", nargs='+',
-                            help="program description")
-    cmd_parser.add_argument("-u", "--url", nargs=1,
-                            help="program homepage URL")
-    args = parser.parse_args()
-    return args
+    # check if file exist
+    if os.path.isfile(file_path):
+        if override:
+            with open(file_path, mode='w') as f:
+                f.truncate(0)
+        else:
+            ans = input('File exist. Override? [ Y | n ]: ')
+            if ans.lower() == 'n':
+                sys.exit('File exist. Exiting...')
+            elif ans.lower() == 'y':
+                with open(file_path, mode='w') as f:
+                    f.truncate(0)
+            else:
+                sys.exit('Unknown answer. Exiting...')
 
 
-def program_dict(cli_mode, args):
-    """Populate a dictionary with all lines of slack-desc file
-
+def write_file(text, file_path, file_name=''):
+    """Writes text to file
     Parameters:
     ----------
-    cli_mode : {bool}
-        Dictionary populating mode.
-    args : {argparse.Namespace}
-        Argparse Namespace
+    text : {str}
+        Text to be written
+    file_path : {str}
+        Path to output file
+    file_name : {str}, optional
+        Name of the output file.
     Returns
     -------
-    dict
-        Returns a dictionary with all lines of slack-desc file
+    [bool]
+        Returns True if success
     """
 
-    program = dict()
-    # check mode
-    if cli_mode:
-        # commandline mode (default)
-        program['name'] = text_validator(args.name)
-        program['short_desc'] = text_validator(
-            args.short, one_line=True, pkg_name=program['name'])
-        program['desc'] = text_validator(
-            args.description, six_lines=True, pkg_name=program['name'])
-        program['url'] = text_validator(
-            args.url, one_word=True, one_line=True, pkg_name=program['name'])
-    else:
-        # interactive mode
-        program['name'] = user_input(
-            'Program name (single word): ', one_word=True)
-        program['short_desc'] = user_input(
-            'Short description (one line): ', one_line=True, pkg_name=program['name'])
-        program['desc'] = user_input(
-            'Description (up to six lines): ', six_lines=True, pkg_name=program['name'])
-        program['url'] = user_input(
-            'Program homepage URL: ', one_word=True, one_line=True, pkg_name=program['name'])
-    # common part
-    program['header'] = header()
-    program['ruler'] = handy_ruler(program['name'])
-    program['empty'] = ''
+    # path
+    file_path = os.path.join(file_path, file_name)
 
-    # warping text
-    for key in ('short_desc', 'desc', 'url', 'empty'):
-        program[key] = text_warpper(program[key], program['name'], ': ')
-
-    return program
-
-
-def write_file(file_path, dictionary):
-    file_path = os.path.join(file_path, 'slack-desc')
-
-    dict_list = list()
-    dict_list.extend(dictionary['header'])
-    dict_list.extend(dictionary['ruler'])
-    dict_list.extend(dictionary['short_desc'])
-    dict_list.extend(dictionary['empty'])
-    dict_list.extend(dictionary['desc'])
-    # add some empty lines to get total of 11
-    if len(dictionary['desc']) < 6:
-        for _ in range(6 - len(dictionary['desc'])):
-            dict_list.extend(dictionary['empty'])
-    dict_list.extend(dictionary['empty'])
-    dict_list.extend(dictionary['url'])
-    dict_list.extend(dictionary['empty'])
-
-    with open(file_path, mode='w') as slack_desc:
-        for line in dict_list:
-            slack_desc.write(line + '\n')
+    with open(file_path, mode='a') as f:
+        f.write(text + '\n')
+        return True
 
 
 def main():
     args = arguments()
 
+    # Gathering information and storing in dictionary
+    program = dict()
     if (len(sys.argv) == 1) or args.interactive:
-        cli_mode = False
+        # interactive mode: to override or not to override?
+        valid_path(args.output, file_name='slack-desc')
 
-    program = program_dict(cli_mode, args)
-
-    print(program['ruler'])
-
-    if args.output:
-        file_path = os.path.expandvars(args.output)
-        file_path = os.path.expanduser(file_path)
-        file_path = os.path.abspath(file_path)
+        program['name'] = user_input('Program name (single word): ',
+                                     one_word=True)
+        program['short_desc'] = user_input('Short description (one line): ',
+                                           one_line=True,
+                                           pkg_name=program['name'])
+        program['desc'] = user_input('Description (up to six lines): ',
+                                     six_lines=True, pkg_name=program['name'])
+        program['url'] = user_input('Program homepage URL: ',
+                                    one_word=True, one_line=True,
+                                    pkg_name=program['name'])
     else:
-        file_path = ''
+        # cli mode: to override!
+        try:
+            valid_path(args.output, file_name='slack-desc', override=True)
+            text_validator(' '.join(args.name))
+            program['name'] = ' '.join(args.name)
 
-    write_file(file_path, program)
+            text_validator(' '.join(args.short), pkg_name=program['name'])
+            program['short_desc'] = ' '.join(args.short)
+
+            text_validator(' '.join(args.description), six_lines=True,
+                           pkg_name=program['name'])
+            program['desc'] = ' '.join(args.description)
+            text_validator(' '.join(args.url), one_word=True, one_line=True,
+                           pkg_name=program['name'])
+            program['url'] = ' '.join(args.url)
+        except ValueError as error:
+            print(error)
+    # common part
+    program['header'] = header()
+    program['ruler'] = handy_ruler(program['name'])
+    program['empty'] = ''
+
+    # warping some values
+    for key in ('short_desc', 'desc', 'url', 'empty'):
+        program[key] = text_warpper(program[key], program['name'], ': ')
+
+    # there is 6 lines for 'desc' insert missing lines
+    if len(program['desc']) < 6:
+        for _ in range(6 - len(program['desc'])):
+            program['desc'].extend(program['empty'])
+
+    for key in ('header', 'ruler', 'short_desc', 'empty',
+                'desc', 'empty', 'url', 'empty'):
+        for text in program[key]:
+            write_file(text, args.output, file_name='slack-desc')
 
 
 if __name__ == '__main__':
